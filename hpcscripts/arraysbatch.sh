45fe8b313239f6138fb2f7aadc5a9bc355700349
bencardoen <26388277+bencardoen@users.noreply.github.com>
2023-02-21 16:22:22 +0000

    Update arraysbatch.sh

--- hpcscripts/arraysbatch.sh
+++ hpcscripts/arraysbatch.sh
@@ -44,7 +44,7 @@ ODIR=$(sed -n "${SLURM_ARRAY_TASK_ID}p" out.txt)
 
 # Test alphas
 IMAGE="$SLURM_TMPDIR/mcsdetect.sif"
-LSRC="/opt/SubPrecisionContactDetection.jl"
+P="/opt/SubPrecisionContactDetection.jl"
 
 mkdir -p $ODIR
 for ALPHA in 0.001 0.01 0.025 0.05
@@ -52,7 +52,7 @@ do
     echo "Starting with alpha $ALPHA"
     mkdir -p $ODIR/$ALPHA
     NOW=$(date +"%m_%d_%Y_HH%I_%M")
-    singularity exec $IMAGE julia --project=$LSRC --sysimage=$LSRC/sys_img.so $LSRC/src/ercontacts.jl  --inpath $IDIR -r "*[1,2].tif" -w 2 --deconvolved --sigmas 2.5-2.5-1.5 --outpath  $ODIR/$ALPHA --alpha $ALPHA --beta  $ALPHA -c 1 -v 2000 --mode=decon 2>&1 | tee -a log_$NOW.txt
+    singularity exec $IMAGE julia --project=$P --sysimage=$P/sys_img.so $P/scripts/ercontacts.jl  --inpath $IDIR -r "*[1,2].tif" -w 2 --deconvolved --sigmas 2.5-2.5-1.5 --outpath  $ODIR/$ALPHA --alpha $ALPHA --beta  $ALPHA -c 1 -v 2000 --mode=decon 2>&1 | tee -a log_$NOW.txt
 done
 NOW=$(date +"%m_%d_%Y_HH%I_%M")
 
