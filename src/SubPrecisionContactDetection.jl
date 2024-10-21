24d13d5b0ce9735dcd81c3af94c7fe1c29866def
Ben Cardoen <bcardoen@sfu.ca>
2024-10-20 18:47:18 -0700

    Refactor contact script

--- src/SubPrecisionContactDetection.jl
+++ src/SubPrecisionContactDetection.jl
@@ -1168,16 +1168,6 @@ end
 
 
 function twochannelcontacts(parsed_args, tiffiles=nothing)
-    # date_format = "yyyy-mm-dd HH:MM:SS"
-    # timestamp_logger(logger) = TransformerLogger(logger) do log
-    #   merge(log, (; message = "$(Dates.format(now(), date_format)) $(basename(log.file)):$(log.line): $(log.message)"))
-    # end
-    # ConsoleLogger(stdout, Logging.Info) |> timestamp_logger |> global_logger
-    # parsed_args = parse_commandline()
-    # @info "Parsed arguments:"
-    # for (arg,val) in parsed_args
-        # @info "  $arg  =>  $val"
-    # end
     dimension = parsed_args["dimension"]
     if dimension != 3
         if dimension != 2
