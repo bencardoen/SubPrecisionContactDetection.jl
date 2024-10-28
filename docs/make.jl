280ca851ee15db896a07082fb03468c195436b29
Ben Cardoen <bcardoen@sfu.ca>
2024-10-28 15:08:57 +0000

    Simplify workflow

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,3 @@
-using Documenter, SubPrecisionContactDetection
+using Documenter
 push!(LOAD_PATH,"../src/")
-makedocs(sitename="SubPrecisionContactDetection Documentation",  pages=["API" => "functions.md", 
-"Tutorial" => "tutorial.md", "Parameter selection" => "parameters.md", "Cluster Usage" => "clustercomputing.md", "Installation" => "installation.md"])
+makedocs(sitename="SubPrecisionContactDetection Documentation",  pages=[ "Tutorial" => "tutorial.md", "Parameter selection" => "parameters.md", "Cluster Usage" => "clustercomputing.md", "Installation" => "installation.md"])
