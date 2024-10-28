362ccb76b82f2d862ecb723eb481cf60abb838f0
Ben Cardoen <bcardoen@sfu.ca>
2024-10-28 15:23:19 +0000

    Test deployment

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,5 @@
 using Documenter
 push!(LOAD_PATH,"../src/")
 makedocs(sitename="SubPrecisionContactDetection Documentation",  pages=[ "Tutorial" => "tutorial.md", "Parameter selection" => "parameters.md", "Cluster Usage" => "clustercomputing.md", "Installation" => "installation.md"])
+
+deploydocs(repo = "github.com/bencardoen/SubPrecisionContactDetection.jl.git")
